6fb402a41659fb3e40b04e0ee03277e905216a89
Djazy-Faradj <djazy.faradj@outlook.com>
2024-10-20 23:52:35 -0400

    Updated comments

--- reader.py
+++ reader.py
@@ -2,7 +2,7 @@
 File: reader.py
 Author: Djazy Faradj
 Created: 09-10-2024
-Last modified: 16-10-2024
+Last modified: 20-10-2024
 Description: Reader.py is a script which takes in an image of a qr code (for now, unaltered, 'perfect' qr image), manipulates it a little to then
 read each pixel of the code and automatically adapts to varying image sizes as it first calls a funciton CalculateCellSize() in which it will determine the size of
 each cell inside that qr code image. From there, it will read the qr code line by line and will then be processed by the QrRead() function.
